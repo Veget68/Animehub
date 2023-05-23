0a090cd222baed635ceacca9eccb9970aec84c41
Veget68 <130899142+Veget68@users.noreply.github.com>
2023-05-24 03:00:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegram.me/Anime_x_,M'>Click here</a>\n○ Anime Channel : @Anime_x_Max\n○ Anime Requests Group : @AnimeMax_7</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegram.me/Anime_x_Max'>Click here</a>\n○ Movie & Series Channel : @Netflix_x_Hub\n○ Movie & Series Requests Group : @Movies_and_Series_x_Request</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
